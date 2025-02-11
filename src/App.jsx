4997162acc7923e82ea5ed4997ee72d2f7360f7c
rtblanco <ronnytoribio1@hotmail.com>
2025-02-10 23:33:09 -0500

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Form from './Form'
 
 const user = {
   name: 'Tom Cook',
@@ -172,7 +173,9 @@ export default function App() {
           </div>
         </header>
         <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <Form/>
+          </div>
         </main>
       </div>
     </>
